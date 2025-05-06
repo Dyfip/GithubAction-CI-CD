488bf26429091381eba2ff624d24a3b85954d077
Dyfip <138641117+Dyfip@users.noreply.github.com>
2025-05-06 17:10:53 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@ import './App.css'
 
 function App() {
   return (
-    <div><h1>Jaspreet's React App</h1></div>
+    <div><h1>Jaspreet's React App</h1>
+      <h2>Welcome..</h2>
+    </div>
   )
 }
 
